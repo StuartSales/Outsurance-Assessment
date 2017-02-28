a13175a514fa1d653510ef1239ba05dd1a0dca54
StuartSales <ssales@argility.com>
2017-02-28 10:27:26 +0200

    Add files via upload

---
+++ AssessmentUnitTest/Assessment_UnitTest.cs
@@ -0,0 +1,159 @@
+﻿using CSVFileProcess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AssessmentUnitTest
+{
+    [TestClass]
+    public class Assessment_UnitTest
+    {
+
+        #region Variables
+
+        readonly string FILE_NAME__IMPORT = "ImportFile.CSV";
+        readonly string FILE_NAME__EXPORT__NAME_FREQUENCY = "Export_NameFrequency.txt";
+        readonly string FILE_NAME__EXPORT__ADDRESS = "Export_Addresses.txt";
+
+        /// <summary>Assert result.</summary>
+        class AssertResult
+        {
+
+            #region Constructor
+
+            public AssertResult()
+            {
+                Condition = true;
+                Message = string.Empty;
+            }
+
+            #endregion
+
+            #region Public Members
+
+            public bool Condition;
+
+            public string Message;
+
+            #endregion
+
+        }
+
+        #endregion
+
+        #region Test Members
+
+        /// <summary>Generate the export files for the unit tests.</summary>
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var processCSVFile = new ProcessCSVFile();
+            processCSVFile.ProcessFile(FILE_NAME__IMPORT, FILE_NAME__EXPORT__NAME_FREQUENCY, FILE_NAME__EXPORT__ADDRESS);
+            processCSVFile = null;
+        }
+
+        /// <summary>Unit test for the "Name Frequency" export.</summary>
+        [TestMethod]
+        public void TestMethod_NameFrequency()
+        {
+            List<string> expectedDataSet = GetExpectedData_NameFrequency();
+            AssertResult assertResult = ValidateExportAndExpectedData(FILE_NAME__EXPORT__NAME_FREQUENCY, expectedDataSet);
+            Assert.IsTrue(assertResult.Condition, assertResult.Message);
+        }
+
+        /// <summary>Unit test for the "Address" export.</summary>
+        [TestMethod]
+        public void TestMethod_Address()
+        {
+            List<string> expectedDataSet = GetExpectedData_Address();
+            AssertResult assertResult = ValidateExportAndExpectedData(FILE_NAME__EXPORT__ADDRESS, expectedDataSet);
+            Assert.IsTrue(assertResult.Condition, assertResult.Message);
+        }
+
+        #endregion
+
+        #region Private Members
+
+        /// <summary>Gets the expected "Name Frequency" results based on the "ImportFile.CSV" file data.</summary>
+        /// <returns>Returns the expected "Name Frequency" results based on the "ImportFile.CSV" file data.</returns>
+        List<string> GetExpectedData_NameFrequency()
+        {
+            var result = new List<string>
+            {
+                "Johnson, 2",
+                "Brown, 1",
+                "Heinrich, 1",
+                "Jones, 1",
+                "Matt, 1",
+                "Smith, 1",
+                "Tim, 1",
+            };
+            return result;
+        }
+
+        /// <summary>Gets the expected "Address" results based on the "ImporetFile.CSV" file data.</summary>
+        /// <returns>Returns the expected "Address" results based on the "ImporetFile.CSV" file data.</returns>
+        List<string> GetExpectedData_Address()
+        {
+            var result = new List<string>
+            {
+                "12 Acton St",
+                "95 Berger St",
+                "31 Clifton Rd",
+                "22 Jones Rd",
+            };
+            return result;
+        }
+
+        /// <summary>Validates that the exported data matches up with the expected data.</summary>
+        /// <param name="fileName">File name of exported data.</param>
+        /// <param name="expectedDataSet">Expected data list.</param>
+        /// <returns>Returns the validation assert result.</returns>
+        AssertResult ValidateExportAndExpectedData(string fileName, List<string> expectedDataSet)
+        {
+            var result = new AssertResult();
+            if (File.Exists(fileName))
+            {
+                var exportedDataSet = new List<string>();
+                // Load the Export Results into a list
+                using (StreamReader streamReader = new StreamReader(fileName))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        exportedDataSet.Add(streamReader.ReadLine());
+                    }
+                }
+                // Validate the lengths
+                if (exportedDataSet.Count == expectedDataSet.Count)
+                {
+                    // Validate the export entries one record at a time
+                    for (int i = 0; i < expectedDataSet.Count; i++)
+                    {
+                        if (exportedDataSet[i] != expectedDataSet[i])
+                        {
+                            result.Condition = false;
+                            result.Message = string.Format("Entry '{0}' is invalid. '{1}' was exported and '{2}' was expected!"
+                                , i, exportedDataSet[i], expectedDataSet[i]);
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    result.Condition = false;
+                    result.Message = string.Format("Number of exported records are invalid, '{0}' records exported and '{1}' was expected!"
+                        , exportedDataSet.Count, expectedDataSet.Count);
+                }
+            }
+            else
+            {
+                result.Condition = false;
+                result.Message = string.Format("Export file '{0}' is not found!", fileName);
+            }
+            return result;
+        }
+
+        #endregion
+
+    }
+}
---
+++ CSVFileProcess/ProcessCSVFile.cs
@@ -0,0 +1,217 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CSVFileProcess
+{
+    public class ProcessCSVFile
+    {
+
+        #region Variables
+
+        /// <summary>Import data information.</summary>
+        class ImportData
+        {
+
+            /// <summary>First Name.</summary>
+            public string FirstName;
+
+            /// <summary>Last Name.</summary>
+            public string LastName;
+
+            /// <summary>Address.</summary>
+            public string Address;
+
+        }
+
+        #endregion
+
+        #region Public Members
+
+        /// <summary>Process the import CSV file and export two results to two output files.</summary>
+        /// <param name="ImportCSVFileName">CSV file to be processed.</param>
+        /// <param name="NameFrequencyFileName">Output file name - name frequency.</param>
+        /// <param name="AddressesFileName">Output file name - addresses.</param>
+        public void ProcessFile(string ImportCSVFileName, string NameFrequencyFileName, string AddressesFileName)
+        {
+            List<ImportData> importDataSet = null;
+            // Import the CSV file data
+            importDataSet = ImportCSVFileImformation(ImportCSVFileName);
+            if (importDataSet.Any())
+            {
+                // Generate the "Name Frequency" export
+                GenerateNameFrequencyExport(NameFrequencyFileName, importDataSet);
+                // Generate the "Address" export
+                GenerateAddressExport(AddressesFileName, importDataSet);
+            }
+        }
+
+        #endregion
+
+        #region Private Members
+
+        /// <summary>Import the CSV file information into a class structure.</summary>
+        /// <param name="fileName">CSV file name to be imported.</param>
+        /// <returns>Returns the imported CSV file data.</returns>
+        List<ImportData> ImportCSVFileImformation(string fileName)
+        {
+            List<ImportData> result = null;
+            if (File.Exists(fileName))
+            {
+                result = new List<ImportData>();
+                using (StreamReader streamReader = new StreamReader(fileName))
+                {
+                    ImportData importData;
+                    while (!streamReader.EndOfStream)
+                    {
+                        importData = ProcessImportDataLine(streamReader.ReadLine());
+                        if (importData != null)
+                        {
+                            result.Add(importData);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException(string.Format("CSV import file '{0}' was nout found!" , fileName));
+            }
+            return result;
+        }
+
+        /// <summary>Process the import data line.</summary>
+        /// <param name="dataLine">Data line to be processed.</param>
+        /// <returns>Returns the imported data information.</returns>
+        ImportData ProcessImportDataLine(string dataLine)
+        {
+            if (string.IsNullOrEmpty(dataLine)) return null;
+            string[] importData = dataLine.Split(',');
+            // Validate the data length and starting value
+            if ((importData.Length != 3) || (importData[0].ToUpper() == "FIRSTNAME")) return null;
+            // Build the resonse data
+            var result = new ImportData
+            {
+                FirstName = importData[0],
+                LastName = importData[1],
+                Address = importData[2],
+            };
+            return result;
+        }
+
+        /// <summary>Generate the "Name Frequency" export file.</summary>
+        /// <param name="exportFileName">Export file name.</param>
+        /// <param name="importDataSet">Import data set.</param>
+        void GenerateNameFrequencyExport(string exportFileName, List<ImportData> importDataSet)
+        {
+            var exportList = new List<string>();
+            // Get the name frequency
+            var nameFrequencySet = new Dictionary<string, int>();
+            foreach (ImportData importData in importDataSet)
+            {
+                CheckNameFrequency(nameFrequencySet, importData.FirstName);
+                CheckNameFrequency(nameFrequencySet, importData.LastName);
+            }
+            // Order the names
+            if (nameFrequencySet.Any())
+            {
+                // Frequency Group
+                List<int> frequencySet = nameFrequencySet.OrderByDescending(n => n.Value).Select(n => n.Value).Distinct().ToList();
+                List<string> nameSet;
+                foreach (int frequency in frequencySet)
+                {
+                    // Name Group
+                    nameSet = nameFrequencySet.Where(n => n.Value == frequency).OrderBy(n => n.Key).Select(n => n.Key).ToList();
+                    foreach (string name in nameSet)
+                    {
+                        exportList.Add(string.Format("{0}, {1}", name, frequency));
+                    }
+                }
+            }
+            // Export the ordered names
+            if (exportList.Any())
+            {
+                GenerateExportFile(exportFileName, exportList);
+            }
+        }
+
+        /// <summary>Checks the name frequency.</summary>
+        /// <param name="frequencySet">Name frequency dictionary.</param>
+        /// <param name="name">Name to be checked.</param>
+        void CheckNameFrequency(Dictionary<string, int> frequencySet, string name)
+        {
+            if (frequencySet.ContainsKey(name))
+            {
+                frequencySet[name] += 1;
+            }
+            else
+            {
+                frequencySet.Add(name, 1);
+            }
+        }
+
+        /// <summary>Generated the "Address" export file.</summary>
+        /// <param name="exportFileName">Export file name.</param>
+        /// <param name="importDataSet">Imported data set.</param>
+        void GenerateAddressExport(string exportFileName, List<ImportData> importDataSet)
+        {
+            // Get the Adress List
+            List<string> addressSet = importDataSet.Select(i => i.Address).ToList();
+            // Generate sorting address values
+            var addressSortSet = new Dictionary<string, string>();
+            foreach (string address in addressSet)
+            {
+                GenerateSortingAdress(addressSortSet, address);
+            }
+            // Order the Address
+            List<string> exportData = addressSortSet.OrderBy(a => a.Value).Select(a => a.Key).ToList();
+            // Generate the Export File
+            GenerateExportFile(exportFileName, exportData);
+        }
+
+        /// <summary>Generated the sorting address value for an address.</summary>
+        /// <param name="addressSortSet">Adress sort dictionary.</param>
+        /// <param name="address">Address value.</param>
+        void GenerateSortingAdress(Dictionary<string, string> addressSortSet, string address)
+        {
+            if (string.IsNullOrEmpty(address)) return;
+            if (addressSortSet.ContainsKey(address)) return;
+            // Slip the address
+            string[] splitData = address.Split(' ');
+            if (!splitData.Any()) return;
+            // Validate if the first "part" is the address number
+            string sortingAddress;
+            int intValue;
+            if (int.TryParse(splitData[0], out intValue))
+            {
+                // ignore the first "part" in the sorting address.
+                sortingAddress = address.Remove(0, splitData[0].Length + 1);
+            }
+            else
+            {
+                // Use the FULL address as the sorting address
+                sortingAddress = address;
+            }
+            addressSortSet.Add(address, sortingAddress);
+        }
+
+        /// <summary>Generates the export file.</summary>
+        /// <param name="exportFileName">Export file name to generate.</param>
+        /// <param name="exportDataSet">Data set to be exported.</param>
+        void GenerateExportFile(string exportFileName, List<string> exportDataSet)
+        {
+            using (var fileStream = new FileStream(exportFileName, FileMode.Create, FileAccess.ReadWrite))
+            {
+                using (var streamWriter = new StreamWriter(fileStream))
+                {
+                    foreach (string exportData in exportDataSet)
+                    {
+                        streamWriter.WriteLine(exportData);
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
